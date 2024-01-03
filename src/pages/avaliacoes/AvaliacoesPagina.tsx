99dd9aa50faf84fc466ee1bc4ac8ffaeebd94f0e
Fernando Avila <fernandoavilajunior@gmail.com>
2024-01-03 15:51:34 -0300

    troca de dado fixo para mock

--- src/pages/avaliacoes/AvaliacoesPagina.tsx
+++ src/pages/avaliacoes/AvaliacoesPagina.tsx
@@ -2,14 +2,44 @@ import { Link } from "react-router-dom";
 import PaginaBasica from "../../components/PaginaBasica";
 import FiltroAvaliacoes from "../../components/filtros/FiltroAvaliacoes";
 import { useTemplate } from "../../hooks/useTemplate";
+import BotaoFiltro from "../../components/buttons/BotaoFiltro";
+import Grid from "../../components/form/Grid";
+
+const formularios = [
+    {
+        id: 1,
+        nome: "Teste formulario 1",
+        curso: "English course",
+        idioma: "Inglês"
+    },
+    {
+        id: 2,
+        nome: "Teste formulario 2",
+        curso: "English course",
+        idioma: "Inglês"
+    },
+    {
+        id: 3,
+        nome: "Teste formulario 3",
+        curso: "English course",
+        idioma: "Inglês"
+    },
+    {
+        id: 4,
+        nome: "Teste formulario 4",
+        curso: "English course",
+        idioma: "Inglês"
+    },
+    {
+        id: 5,
+        nome: "Teste formulario 5",
+        curso: "English course",
+        idioma: "Inglês"
+    }
+];
 
 export default function AvaliacoesPagina() {
     const { setAreaDestaque, setFiltro } = useTemplate();
-
-    const HandleOpenFilter = (e: any) => {
-        setFiltro(<FiltroAvaliacoes OnHandleProcessar={OnHandleProcessarFiltro} />);
-        setAreaDestaque(true);
-    }
 
     function OnHandleProcessarFiltro(dados: any) {
         console.log('teste com dados nome: ' + dados.avaliacao);
@@ -22,39 +52,35 @@ export default function AvaliacoesPagina() {
                 <h2>Avaliações</h2>
                 <div>
                     <Link to={'/avaliacoes/avaliar'}><button className="btn btn-primary mx-2">Criar avaliação</button></Link>
-                    <button onClick={HandleOpenFilter} className="btn btn-secundary">Filtro</button>
+                    <BotaoFiltro filtro={<FiltroAvaliacoes OnHandleProcessar={OnHandleProcessarFiltro} />} />
                 </div>
             </div>
             <div className="resultados">
-                <table className="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Grid
+                    data={formularios}
+                    renderItem={ item => (
+                        <>
+                            <td scope="row">{ item.id }</td>
+                            <td>{ item.nome }</td>
+                            <td>{ item.curso }</td>
+                            <td>{ item.idioma }</td>
+                        </>
+                    )}
+                    actions={{
+                        previa: {
+                            icone: 'icone',
+                            acao: () => {
+
+                            }
+                        },
+                        remover: {
+                            icone: 'icone',
+                            acao: () => {
+
+                            }
+                        }
+                    }}
+                />    
             </div>
         </PaginaBasica>
     );
