ff18a0ad84f3e082277ba5570403365ef8e2c2d7
Fernando Avila <fernandoavilajunior@gmail.com>
2024-01-03 11:45:11 -0300

    criação pagina avaliar

---
+++ src/pages/avaliacoes/AvaliarPagina.tsx
@@ -0,0 +1,11 @@
+import PaginaBasica from "../../components/PaginaBasica";
+
+export default function AvaliarPagina() {
+    return (
+        <PaginaBasica>
+            <div className="py-4">
+                <h3>Avaliar aula</h3>
+            </div>
+        </PaginaBasica>
+    );
+}
