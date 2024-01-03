f02d03adde83f856ab03c52f613e93199eba553f
Fernando Avila <fernandoavilajunior@gmail.com>
2024-01-03 11:41:38 -0300

    inserção de url no logo para levar à home

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom"
+
 export default function Header() {
     return (
         <header className="d-flex justify-content-center" style={{ padding: '40px 0px', width: '100%' }}>
             <div className="container d-flex justify-content-between">
-                <div className="logo"><b>CCAA</b> | ARA</div>
+                <Link to={'/'}><div className="logo"><b>CCAA</b> | ARA</div></Link>
                 <div className="functions">Logout</div>
             </div>
         </header>
