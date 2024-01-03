5366f479fc698b1a7648db4782101c71164cbb4a
Fernando Avila <fernandoavilajunior@gmail.com>
2024-01-03 14:52:11 -0300

    criação do menu de formularios

--- src/components/common/MenuHeader.tsx
+++ src/components/common/MenuHeader.tsx
@@ -6,6 +6,16 @@ export default function MenuHeader() {
             <ul className="container d-flex justify-content-between">
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/avaliacoes'}>Avaliações</Link></li>
+                <li className="dropdown-menu-parent">
+                    <Link to={'/formularios'}>Formulários</Link>
+                    <ul className="dropdown-menu">
+                        <li><Link to={'/formularios'}>Formulários</Link></li>
+                        <li><Link to={'/formularios/passos'}>Passos</Link></li>
+                        <li><Link to={'/formularios/grupos'}>Grupos</Link></li>
+                        <li><Link to={'/formularios/perguntas'}>Perguntas</Link></li>
+                        <li><Link to={'/formularios/respostas'}>Respostas</Link></li>
+                    </ul>
+                </li>
                 <li><Link to={''}>Relatórios</Link></li>
                 <li><Link to={''}>Usuários</Link></li>
             </ul>
