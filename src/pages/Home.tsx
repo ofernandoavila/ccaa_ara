e5c1cc282a381bd06fe3ed0b3121d567806a7ca0
Fernando Avila <fernandoavilajunior@gmail.com>
2024-01-03 11:45:00 -0300

    ajustes de filtro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
 import PaginaBasica from "../components/PaginaBasica";
+import FiltroUsuarios from "../components/filtros/FiltroUsuarios";
+import AreaLateral from "../components/popup/AreaLateral";
+import { useTemplate } from "../hooks/useTemplate";
 
 export default function Home() {
+
+    const { setAreaDestaque, setFiltro } = useTemplate();
+
+    const HandleOpenFilter = (e: any) => {
+        setFiltro(<FiltroUsuarios OnHandleProcessar={OnHandleProcessarFiltro} />);
+        setAreaDestaque(true);
+    }
+
+    function OnHandleProcessarFiltro() {
+        setAreaDestaque(false);
+    }
+
     return (
         <PaginaBasica>
-            <h2>Home</h2>
+            <div className="d-flex justify-content-between py-4">
+                <h2>Home</h2>
+                <button onClick={HandleOpenFilter} className="btn btn-primary">Abrir filtro</button>
+            </div>
         </PaginaBasica>
     );
 }
--- src/pages/avaliacoes/AvaliacoesPagina.tsx
+++ src/pages/avaliacoes/AvaliacoesPagina.tsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
 import PaginaBasica from "../../components/PaginaBasica";
+import FiltroAvaliacoes from "../../components/filtros/FiltroAvaliacoes";
+import { useTemplate } from "../../hooks/useTemplate";
 
 export default function AvaliacoesPagina() {
+    const { setAreaDestaque, setFiltro } = useTemplate();
+
+    const HandleOpenFilter = (e: any) => {
+        setFiltro(<FiltroAvaliacoes OnHandleProcessar={OnHandleProcessarFiltro} />);
+        setAreaDestaque(true);
+    }
+
+    function OnHandleProcessarFiltro(dados: any) {
+        console.log('teste com dados nome: ' + dados.avaliacao);
+        setAreaDestaque(false);
+    }
+
     return (
         <PaginaBasica>
-            <h3>Avaliações</h3>
+            <div className="d-flex justify-content-between py-4">
+                <h2>Avaliações</h2>
+                <div>
+                    <Link to={'/avaliacoes/avaliar'}><button className="btn btn-primary mx-2">Criar avaliação</button></Link>
+                    <button onClick={HandleOpenFilter} className="btn btn-secundary">Filtro</button>
+                </div>
+            </div>
             <div className="resultados">
                 <table className="table">
                     <thead>
